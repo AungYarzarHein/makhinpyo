15da8376bafb0f6843b4f57a045db46483c508a1
Yarzar <aungyarzarhein16@gmial.com>
2024-05-02 01:46:43 +0630

    first

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import "dotenv/config";
+
+// import { db } from "./firebase_config.js";
+// import { Firestore, collection, doc, getDoc, getDocs } from "firebase/firestore";
+
+
+const app = express();
+
+app.use(express.static("public"));
+// app.set("view engine","ejs") ;
+// app.set("views","views")
+
+
+app.get("/",async (req,res) => {
+    // const headerData = await getDoc(doc(db,"post-header","post1"))
+    // const data = await getDoc(doc(db,"details","post1")) ;
+   
+    // res.render("index",{data:data.data().data , headerData:headerData.data()});
+    res.send("Hello MaKhinPyo")
+
+})
+
+
+// app.get("/post/:dayNumber/:postId",(req,res) => {
+//     res.render("post",{dayNumber:req.params.dayNumber,postId:req.params.postId})
+// })
+
+
+
+
+app.listen(process.env.PORT);
