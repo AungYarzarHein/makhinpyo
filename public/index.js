ffac446c5f87900738952739c258e8caf6102345
Yarzar <aungyarzarhein16@gmial.com>
2024-05-07 23:57:09 +0630

    change a little

--- public/index.js
+++
@@ -1,9 +0,0 @@
-console.log("Javascript from client side");
-const btn = document.getElementById("btn");
-console.log(btn.innerText)
-
-const onPressHandler = () => {
-    window.location.href= "http://localhost:3000/post/22/2121"
-}
-
-btn.addEventListener("click",onPressHandler);
